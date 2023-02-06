442a87d8826ddd1a0820bacf3b821cb577a677ee
DazzDev <david.amaral2001@gmail.com>
2023-02-06 17:14:07 +0000

    playerId e  cores

--- commands.ts
+++ commands.ts
@@ -3,15 +3,16 @@ import { room } from "./index";
 export function checkAndHandleCommands(player: PlayerObject, message: string): boolean {
     if (!isCommand(message)) return false;
     const command = message.substring(1);
+    const playerId = player.id;
     switch (command) {
         case "help":
-            room.sendAnnouncement("❓ Comandos: !help - mostrar esta mensagem; !bb - sair da sala.", player.id, 0xFFFFFF, "bold", 0);
+            room.sendAnnouncement("❓ Comandos: !help - mostrar esta mensagem; !bb - sair da sala.", playerId, 0xFFFFFF, "bold", 0);
             break;
         case "bb":
-            room.kickPlayer(player.id, "Comando !bb", false);
+            room.kickPlayer(playerId, "Comando !bb", false);
             break;
         default:
-            room.sendAnnouncement("🚫 Esse comando não existe.", player.id, 0xFF0000, "bold", 0);
+            room.sendAnnouncement("🚫 Esse comando não existe.", playerId, 0xFF0000, "bold", 0);
             break;
     }
     return true;
--- playerjoining.ts
+++ playerjoining.ts
@@ -6,7 +6,7 @@ export function handlePlayerJoining(player: PlayerObject): void {
     const playerList: PlayerObject[] = room.getPlayerList();
     if (isPlayerAlreadyConnected(playerId, player.conn)) return;
     if (adminAuthList.has(player.auth)) room.setPlayerAdmin(playerId, true);
-    room.sendAnnouncement(`👋 Bem-vindo, ${player.name}. Escreve !help para veres a lista dos comandos.`, player.id, 0x00D100, "bold", 0);
+    room.sendAnnouncement(`👋 Bem-vindo, ${player.name}. Escreve !help para veres a lista dos comandos.`, playerId, 0x00FF00, "bold", 0);
     specPlayerIdList.push(playerId);
     checkAndRestartWithNewMode(playerList.length);
 }
