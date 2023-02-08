590146396f091bcbb30949bed71654c9466b9771
DazzDev <david.amaral2001@gmail.com>
2023-02-08 15:34:45 +0000

    token

--- index.ts
+++ index.ts
@@ -37,7 +37,7 @@ HaxballJS.then((HBInit) => {
       lat: 41.15144214309606,
       lon: -8.613879659626768
     },
-    token: "thr1.AAAAAGPjpNFNYhVxeCzLzA.TNttVRlQLjU", //https://haxball.com/headlesstoken
+    token: "thr1.AAAAAGPjwQUsHYByErdDZA.AYdO9zxfod8", //https://haxball.com/headlesstoken
   });
 
   room.setScoreLimit(scoreLimit);
